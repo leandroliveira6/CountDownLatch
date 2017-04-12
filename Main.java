e7873fbdb497f3a9ae68b48fdf1358f26beba29c
Leandro <carlosl.cp@hotmail.com>
2017-04-12 11:30:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main{
     public static void main(String args[]) throws InterruptedException{
     	CountDownLatch trava = new CountDownLatch(5);
         for (int i=1; i<6; i++){
-        	Tarefa t = new Tarefa(trava, i*100);
-        	Thread.sleep(1000); //InterruptedException
+            Tarefa t = new Tarefa(trava, i*100);
+            Thread.sleep(1000); //InterruptedException
             new Thread(t).start();
         }
     }
