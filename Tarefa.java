b0d5750b0d37fa1e702116d3a57656a2c55bb75c
Leandro <carlosl.cp@hotmail.com>
2017-04-12 11:32:48 -0300

    Update Tarefa.java

--- Tarefa.java
+++ Tarefa.java
@@ -4,23 +4,24 @@ import java.util.concurrent.CountDownLatch;
 
 class Tarefa implements Runnable{
         CountDownLatch trava;
-        int id;
+	int id;
  
-        public Tarefa(CountDownLatch trava, int id){
-            this.trava = trava;
-            this.id = id;
+	public Tarefa(CountDownLatch trava, int id){
+		this.trava = trava;
+		this.id = id;
         }
  
         public void run(){
-        	trava.countDown(); //decrementa count
-        	System.out.println("["+id+"] Tarefa iniciada! (DEBUG GETCOUNT()="+trava.getCount()+")");
-        	try {
-				trava.await(); //InterruptedException
-				System.out.println("["+id+"] Rodando...");
-	            Thread.sleep(id*10); //InterruptedException
-	            System.out.println("["+id+"] Terminou!");
-			} catch (InterruptedException e) {
-				System.exit(1);
-			}
-        }
-    }
+		trava.countDown(); //decrementa count
+		System.out.println("["+id+"] Tarefa iniciada! (DEBUG GETCOUNT()="+trava.getCount()+")");
+		try {
+			trava.await(); //InterruptedException
+			System.out.println("["+id+"] Rodando...");
+			Thread.sleep(id*10); //InterruptedException
+			System.out.println("["+id+"] Terminou!");
+			} 
+		catch (InterruptedException e) {
+			System.exit(1);
+		}
+	}
+}
